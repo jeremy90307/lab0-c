d5960d3fb00b93b8363954ae85bf07d82ede289d
Jeremy <jeremy90307@gmail.com>
2024-03-01 01:02:36 +0800

    Add q_descend

--- queue.c
+++ queue.c
@@ -14,19 +14,21 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    struct list_head *new = (struct list_head *)malloc(sizeof(struct list_head));
-    if(!new)
+    struct list_head *new =
+        (struct list_head *) malloc(sizeof(struct list_head));
+    if (!new)
         return NULL;
     INIT_LIST_HEAD(new);
     return new;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {
-    if(!head)
+void q_free(struct list_head *head)
+{
+    if (!head)
         return;
     element_t *node, *temp;
-    list_for_each_entry_safe(node, temp, head, list){
+    list_for_each_entry_safe (node, temp, head, list) {
         list_del(&node->list);
         free(node);
     }
@@ -35,13 +37,13 @@ void q_free(struct list_head *head) {
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
-    if(!head)
+    if (!head)
         return false;
     element_t *node = malloc(sizeof(element_t));
-    if(!node)
+    if (!node)
         return false;
     node->value = strdup(s);
-    if(!node->value){
+    if (!node->value) {
         free(node);
         return false;
     }
@@ -52,13 +54,13 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    if(!head)
+    if (!head)
         return false;
     element_t *node = malloc(sizeof(element_t));
-    if(!node)
+    if (!node)
         return false;
     node->value = strdup(s);
-    if(!node->value)
+    if (!node->value)
         return false;
     list_add_tail(&node->list, head);
     return true;
@@ -67,10 +69,10 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    if(!head)
+    if (!head)
         return NULL;
-    element_t *node = list_first_entry(head, element_t, list); //獲取第一個節點
-    if(!sp && bufsize <= 0)
+    element_t *node = list_first_entry(head, element_t, list);  //獲取第一個節點
+    if (!sp && bufsize <= 0)
         return NULL;
     strncpy(sp, node->value, bufsize);
     list_del(&node->list);
@@ -80,10 +82,10 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if(!head)
+    if (!head)
         return NULL;
     element_t *node = list_last_entry(head, element_t, list);
-    if(!sp && bufsize <= 0)
+    if (!sp && bufsize <= 0)
         return NULL;
     strncpy(sp, node->value, bufsize);
     list_del(&node->list);
@@ -93,11 +95,11 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    int sum=0;
-    if(!head)
+    int sum = 0;
+    if (!head)
         return 0;
     element_t *node = malloc(sizeof(element_t));
-    list_for_each_entry(node, head, list){
+    list_for_each_entry (node, head, list) {
         sum++;
     }
     return sum;
@@ -107,16 +109,16 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
-    if(head == NULL || head->next == NULL)
+    if (head == NULL || head->next == NULL)
         return false;
     struct list_head *fast = head;
     struct list_head *slow = head;
-    while(fast != NULL && fast->next !=NULL){
+    while (fast != NULL && fast->next != NULL) {
         fast = fast->next->next;
         slow = slow->next;
     }
-    list_del(slow); //prev->next = slow->next;
-    q_release_element(list_entry(slow, element_t, list)); //參考日升學長
+    list_del(slow);  // prev->next = slow->next;
+    q_release_element(list_entry(slow, element_t, list));  //參考日升學長
     return true;
 }
 
@@ -131,22 +133,23 @@ bool q_delete_dup(struct list_head *head)
 void q_swap(struct list_head *head)
 {
     // https://leetcode.com/problems/swap-nodes-in-pairs/
-    if(head == NULL)
+    if (head == NULL)
         return;
     struct list_head *cur = head->next;
     struct list_head *newhead = cur;
-    for(;cur != head && cur->next != head;
-        cur = cur->next,newhead = newhead->next->next){
-        list_move(cur,newhead->next);
+    for (; cur != head && cur->next != head;
+         cur = cur->next, newhead = newhead->next->next) {
+        list_move(cur, newhead->next);
     }
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {
-    if(!head || head->next == NULL)
+void q_reverse(struct list_head *head)
+{
+    if (!head || head->next == NULL)
         return;
     struct list_head *node, *safe;
-    list_for_each_safe(node, safe, head){
+    list_for_each_safe (node, safe, head) {
         node->next = node->prev;
         node->prev = safe;
     };
@@ -162,7 +165,9 @@ void q_reverseK(struct list_head *head, int k)
 }
 
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void q_sort(struct list_head *head, bool descend) {
+
+}
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
@@ -177,7 +182,21 @@ int q_ascend(struct list_head *head)
 int q_descend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if(!head)
+        return;
+    struct list_head *curr = head->prev, *prev = curr->prev;
+    element_t *curr_entry = list_entry(curr, element_t, list);
+    element_t *prev_entry = list_entry(prev, element_t, list);
+    while(prev != head){
+        if(strcmp(curr_entry->value,prev_entry->value)>0){
+            list_del(prev);
+            q_release_element(prev_entry);
+        }
+        else{
+            curr = prev;
+        }
+    }
+    return q_size(head);
 }
 
 /* Merge all the queues into one sorted queue, which is in ascending/descending
